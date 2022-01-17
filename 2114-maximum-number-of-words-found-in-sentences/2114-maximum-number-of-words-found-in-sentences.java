23e5423b45cf318bb140a4a672b4e7ad1ad0a108
Shane Park <psh40963@naver.com>
2022-01-17 21:08:47 +0900

    Time: 5 ms (62.82%), Space: 42.1 MB (43.41%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int mostWordsFound(String[] sentences) {
+        int max = 0;
+        for (String sentence : sentences) {
+            max = Math.max(max, sentence.split(" ").length);
+        }
+        return max;
+    }
+}
