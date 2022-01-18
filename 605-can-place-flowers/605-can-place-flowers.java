4af29ce61ef204beb7bb0d75242d5cb037a51d06
Shane Park <psh40963@naver.com>
2022-01-18 09:41:53 +0900

    Time: 6 ms (8.37%), Space: 49.1 MB (36.91%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.java
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -1,13 +1,18 @@
 class Solution {
- public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
         int previous = 0;
         for (int i = 0; i < flowerbed.length && 0 < n; i++) {
-            if (previous + flowerbed[i] + ((i < flowerbed.length - 1) ? flowerbed[i + 1] : 0) == 0) {
-                n--;
-                previous = 1;
-            } else {
-                previous = flowerbed[i];
+            try {
+                if (previous == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                    n--;
+                    previous = 1;
+                } else {
+                    previous = flowerbed[i];
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return n == 1;
             }
+
         }
         return n == 0;
     }
