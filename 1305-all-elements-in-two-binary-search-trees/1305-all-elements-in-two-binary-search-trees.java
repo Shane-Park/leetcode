1cb16424b04fe5e4d635dcb325fa56e4553d439c
Shane Park <psh40963@naver.com>
2022-01-26 09:19:15 +0900

    Time: 13 ms (88.65%), Space: 41.8 MB (87.21%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,23 +14,34 @@
  * }
  */
 class Solution {
-  public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> list = new ArrayList<>();
-        add(root1, list);
-        add(root2, list);
-        list.sort(Comparator.comparingInt(integer -> integer));
-        return list;
+public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        add(root1, list1);
+        add(root2, list2);
+
+        List<Integer> answer = new ArrayList<>();
+        int i1, i2;
+        for (i1 = 0, i2 = 0; i1 < list1.size() && i2 < list2.size(); ) {
+            if (list1.get(i1) > list2.get(i2)) {
+                answer.add(list2.get(i2++));
+            } else {
+                answer.add(list1.get(i1++));
+            }
+        }
+        if (i1 < list1.size()) {
+            answer.addAll(list1.subList(i1, list1.size()));
+        } else {
+            answer.addAll(list2.subList(i2, list2.size()));
+        }
+        return answer;
     }
 
     private void add(TreeNode tree, List<Integer> list) {
         if (tree == null)
             return;
+        add(tree.left, list);
         list.add(tree.val);
-        if (tree.left != null) {
-            add(tree.left, list);
-        }
-        if (tree.right != null) {
-            add(tree.right, list);
-        }
+        add(tree.right, list);
     }
 }
