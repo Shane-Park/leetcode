ef844021cdc4a3a2fb9d76abe7e7450a6bf63a5b
Shane Park <psh40963@naver.com>
2022-02-12 20:41:37 +0900

    Time: 287 ms (26.10%), Space: 118.1 MB (5.10%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,48 +1,33 @@
 class Solution {
- public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Node> q = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        int cnt = 1;
 
-        q.add(new Node(beginWord, 1));
         while (!q.isEmpty()) {
-            Node poll = q.poll();
-            String word = poll.word;
-            if (word.equals(endWord)) {
-                return poll.depth;
-            }
-
-            for (String s : wordList) {
-                if (!visited.contains(s) && adjacent(word, s)) {
-                    visited.add(s);
-                    q.add(new Node(s, poll.depth + 1));
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String poll = q.poll();
+                if (poll.equals(endWord)) {
+                    return cnt;
+                }
+                set.remove(poll);
+                for (int j = 0; j < beginWord.length(); j++) {
+                    for (int k = 0; k < 26; k++) {
+                        StringBuffer sb = new StringBuffer(poll);
+                        sb.setCharAt(j, (char) ('a' + k));
+                        if (set.contains(sb.toString())) {
+                            q.offer(sb.toString());
+                            set.remove(sb.toString());
+                        }
+                    }
                 }
             }
+            cnt++;
         }
+
         return 0;
-    }
 
-    public class Node {
-        String word;
-        int depth;
-
-        public Node(String word, int depth) {
-            this.word = word;
-            this.depth = depth;
-        }
-
-    }
-
-
-    private boolean adjacent(String word, String s) {
-        int diff = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != s.charAt(i)) {
-                diff++;
-            }
-            if (1 < diff) {
-                return false;
-            }
-        }
-        return diff == 1;
     }
 }
