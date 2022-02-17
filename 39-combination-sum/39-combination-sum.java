3ab35213c33ad3836aa3fe5668e20a51b87f30fa
Shane Park <psh40963@naver.com>
2022-02-17 10:15:58 +0900

    Time: 15 ms (12.81%), Space: 45.3 MB (14.04%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,68 +1,21 @@
 class Solution {
- public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Set<ArrayObject> set = new HashSet<>();
-        ArrayObject arr = new ArrayObject(new int[candidates.length]);
-        dfs(0, arr, set, candidates, target);
-
-        List<List<Integer>> answer = new ArrayList<>();
-        for (ArrayObject arrayObject : set) {
-            List<Integer> list = new ArrayList<>();
-            int[] data = arrayObject.arr;
-            for (int i = 0; i < data.length; i++) {
-                for (int j = 0; j < data[i]; j++) {
-                    list.add(candidates[i]);
-                }
-            }
-            answer.add(list);
-        }
-
-        return answer;
+   public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        bt(list, new Stack<Integer>(), candidates, target, 0);
+        return list;
     }
 
-    private void dfs(int sum, ArrayObject cur, Set<ArrayObject> set, int[] candidates, int target) {
-        if (sum == target) {
-            set.add(cur);
-        }
-        if (sum >= target)
+    private void bt(List<List<Integer>> list, Stack<Integer> temp, int[] candidates, int remain, int start) {
+        if (remain == 0) {
+            list.add(new ArrayList<>(temp));
             return;
-
-        for (int i = 0; i < candidates.length; i++) {
-            ArrayObject temp = new ArrayObject(cur);
-            temp.arr[i]++;
-            dfs(sum + candidates[i], temp, set, candidates, target);
         }
-
-    }
-
-    static class ArrayObject {
-        int[] arr;
-
-        public ArrayObject(int[] arr) {
-            this.arr = arr;
-        }
-
-        public ArrayObject(ArrayObject target) {
-            this.arr = Arrays.copyOf(target.arr, target.arr.length);
-        }
-
-        @Override
-        public String toString() {
-            return "ArrayObject{" +
-                    "arr=" + Arrays.toString(arr) +
-                    '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            ArrayObject that = (ArrayObject) o;
-            return Arrays.equals(arr, that.arr);
-        }
-
-        @Override
-        public int hashCode() {
-            return Arrays.hashCode(arr);
+        if (remain <= 0)
+            return;
+        for (int i = start; i < candidates.length; i++) {
+            temp.push(candidates[i]);
+            bt(list, temp, candidates, remain - candidates[i], i);
+            temp.pop();
         }
     }
 }
