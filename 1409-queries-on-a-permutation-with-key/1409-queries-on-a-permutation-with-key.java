7181cbfa8d33dba57beb23d488afa33911a9160b
Shane Park <psh40963@naver.com>
2022-02-28 12:32:10 +0900

    Time: 18 ms (33.70%), Space: 43.7 MB (15.38%) - LeetHub

---
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            list.add(i + 1);
+        }
+        for (int i = 0; i < queries.length; i++) {
+            int query = queries[i];
+            int index = list.indexOf(query);
+            list.remove(index);
+            list.add(0, query);
+            queries[i] = index;
+        }
+        return queries;
+    }
+}
