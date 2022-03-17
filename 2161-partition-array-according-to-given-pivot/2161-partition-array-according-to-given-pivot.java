8b11cd6da6aa2aaf6e738f95d5dcd246f0fa5c07
Shane Park <psh40963@naver.com>
2022-03-17 21:41:57 +0900

    Time: 17 ms (39.53%), Space: 163.8 MB (66.03%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,11 +1,12 @@
 class Solution {
-     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> smaller = new ArrayList<>();
+   public int[] pivotArray(int[] nums, int pivot) {
+        int[] answer = new int[nums.length];
+        int index = 0;
         int pivotCount = 0;
         List<Integer> bigger = new ArrayList<>();
         for (int num : nums) {
             if (num < pivot) {
-                smaller.add(num);
+                answer[index++] = num;
             } else if (num == pivot) {
                 pivotCount++;
             } else {
@@ -13,9 +14,13 @@ class Solution {
             }
         }
         for (int i = 0; i < pivotCount; i++) {
-            smaller.add(pivot);
+            answer[index++] = pivot;
         }
-        smaller.addAll(bigger);
-        return smaller.stream().mapToInt(Integer::intValue).toArray();
+
+        for (Integer integer : bigger) {
+            answer[index++] = integer;
+        }
+
+        return answer;
     }
 }
