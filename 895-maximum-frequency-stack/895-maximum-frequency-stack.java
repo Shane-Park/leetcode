50c3d8f6fc329f9ad3829e0262f4a4499ad0ad65
Shane Park <psh40963@naver.com>
2022-03-19 11:30:01 +0900

    Time: 39 ms (78.68%), Space: 70.8 MB (37.62%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -1,66 +1,24 @@
- class FreqStack {
-
-        PriorityQueue<Num> pq;
-        Map<Integer, Num> map;
-        int pushCount;
-
-        public FreqStack() {
-            this.pq = new PriorityQueue<>((o1, o2) -> {
-                int size1 = o1.pushIndex.size();
-                int size2 = o2.pushIndex.size();
-                if (size1 == size2) {
-                    return o2.pushIndex.peek() - o1.pushIndex.peek();
-                }
-                return size2 - size1;
-            });
-            this.map = new HashMap<>();
-            this.pushCount = 0;
-        }
+class FreqStack {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Map<Integer, Stack<Integer>> stackMap = new HashMap<>();
+        int maxCount;
 
         public void push(int val) {
-            Num num = map.get(val);
-            if (num == null) {
-                num = new Num(val);
-                map.put(val, num);
-            }
-
-            if (pq.contains(num))
-                pq.remove(num);
-
-            num.pushIndex.push(pushCount++);
-            pq.offer(num);
+            int count = countMap.getOrDefault(val, 0) + 1;
+            countMap.put(val, count);
+            maxCount = Math.max(maxCount, count);
+            stackMap.putIfAbsent(count, new Stack<>());
+            Stack<Integer> stack = stackMap.get(count);
+            stack.add(val);
         }
 
         public int pop() {
-            Num num = pq.poll();
-            num.pushIndex.pop();
-            if (!num.pushIndex.isEmpty())
-                pq.offer(num);
-            return num.val;
+            Stack<Integer> maxStack = stackMap.get(maxCount);
+            Integer val = maxStack.pop();
+            countMap.put(val, maxCount - 1);
+            if (maxStack.isEmpty())
+                maxCount--;
+            return val;
         }
 
-        class Num {
-            int val;
-            Stack<Integer> pushIndex;
-
-            public Num(int number) {
-                this.val = number;
-                this.pushIndex = new Stack<>();
-            }
-
-            @Override
-            public String toString() {
-                return "Num{" +
-                        "val=" + val +
-                        ", pushIndex=" + pushIndex +
-                        '}';
-            }
-        }
     }
-
-/**
- * Your FreqStack object will be instantiated and called as such:
- * FreqStack obj = new FreqStack();
- * obj.push(val);
- * int param_2 = obj.pop();
- */
