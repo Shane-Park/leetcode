e4abac97f6b8e9555f77b42f120aaac3667e26a1
Shane Park <psh40963@naver.com>
2022-03-20 17:07:33 +0900

    Time: 5 ms (79.08%), Space: 93.6 MB (38.97%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,44 +1,22 @@
 class Solution {
- public int minDominoRotations(int[] tops, int[] bottoms) {
-        int[] cnt = new int[6];
-        for (int top : tops) {
-            cnt[top - 1]++;
-        }
-        for (int bottom : bottoms) {
-            cnt[bottom - 1]++;
-        }
-
-        int n = -1;
-        for (int i = 0; i < 6; i++) {
-            if (cnt[i] >= tops.length) {
-                n = i;
-            }
-        }
-        if (n < 0) {
-            return n;
-        }
-
-        n++;
-
-        int topCnt = 0;
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int[] topCnt = new int[7];
+        int[] bottomCnt = new int[7];
+        int[] bothCnt = new int[7];
         for (int i = 0; i < tops.length; i++) {
-            if (tops[i] == n && bottoms[i] != n) {
-                topCnt++;
-            } else if (bottoms[i] == n && tops[i] != n) {
-                topCnt--;
-            } else if (tops[i] != n && bottoms[i] != n) {
-                return -1;
+            topCnt[tops[i]]++;
+            bottomCnt[bottoms[i]]++;
+            if (tops[i] == bottoms[i]) {
+                bothCnt[tops[i]]++;
             }
         }
 
-        int count = 0;
-        int[] main = topCnt > 0 ? tops : bottoms;
-        for (int i = 0; i < tops.length; i++) {
-            if (main[i] != n) {
-                count++;
+        for (int i = 1; i <= 6; i++) {
+            if (topCnt[i] + bottomCnt[i] == tops.length + bothCnt[i]) {
+                return Math.min(tops.length - topCnt[i], tops.length - bottomCnt[i]);
             }
         }
 
-        return count;
+        return -1;
     }
 }
