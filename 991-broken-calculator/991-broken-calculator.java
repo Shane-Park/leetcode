fb1d37c50e837e73d12938140e104cd43c4b41fc
Shane Park <psh40963@naver.com>
2022-03-23 12:57:31 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (26.69%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.java
+++ 991-broken-calculator/991-broken-calculator.java
@@ -1,13 +1,10 @@
 class Solution {
     public int brokenCalc(int startValue, int target) {
         int cnt = 0;
-        while (startValue != target) {
-            if (target > startValue && target % 2 == 0)
-                target = target / 2;
-            else
-                target++;
+        while (target > startValue) {
+            target = target % 2 == 0 ? target / 2 : target + 1;
             cnt++;
         }
-        return cnt;
+        return cnt + (startValue - target);
     }
 }
