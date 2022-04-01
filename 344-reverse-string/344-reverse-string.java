4546693993404e67fb67891816113f6cc61ce948
Shane Park <psh40963@naver.com>
2022-04-01 09:07:39 +0900

    Time: 0 ms (100.00%), Space: 49.2 MB (86.52%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,9 @@
+class Solution {
+
+    public void reverseString(char[] s) {
+        char[] copy = Arrays.copyOf(s, s.length);
+        for (int i = 0; i < s.length; i++) {
+            s[i] = copy[s.length - 1 - i];
+        }
+    }
+}
